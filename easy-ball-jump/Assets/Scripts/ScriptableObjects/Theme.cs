5e04cb3ebc29f39882c7d8a317245b5bde0ca541
krisdaniel <krisdaniel1452@gmail.com>
2020-06-15 17:30:05 +0300

    Add Theme Scriptable Objects

---
+++ easy-ball-jump/Assets/Scripts/ScriptableObjects/Theme.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+namespace ScriptableObjects {
+    [CreateAssetMenu(fileName = "Theme", menuName = "Game Theme", order = 0)]
+    public class Theme : ScriptableObject {
+        public Color BackgroundColor;
+        public Color BallColor;
+    }
+}
